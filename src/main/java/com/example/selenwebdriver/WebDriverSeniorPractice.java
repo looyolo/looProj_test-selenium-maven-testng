dbbf544393877233068955a74542112206336d3b
looyolo@qq.com <looyolo@qq.com>
2021-05-07 16:19:33 +0800

    新增 DataProvider 数据驱动类 示例

--- src/main/java/com/example/selenwebdriver/WebDriverSeniorPractice.java
+++ src/main/java/com/example/selenwebdriver/WebDriverSeniorPractice.java
@@ -66,16 +66,16 @@ public class WebDriverSeniorPractice {
 
     @Parameters({"browser", "whichDriver" , "whereDriver"})
     @BeforeClass
-    public void setUp(String browser, String whichDriver , String whereeDriver) {
+    public void setUp(String browser, String whichDriver , String whereDriver) {
         if (browser.equalsIgnoreCase("firefox")) {
             // 指定 浏览器驱动程序 的 存放路径，并添加到系统属性中
-            System.setProperty(whichDriver, whereeDriver);
+            System.setProperty(whichDriver, whereDriver);
             driver = new FirefoxDriver();
             // 打开浏览器，使用"无头模式"，不会在桌面上打开，只在内存上运行
 //            driver = new FirefoxDriver(new FirefoxOptions().addArguments("-headless"));
         }
         if (browser.equalsIgnoreCase("chrome")) {
-            System.setProperty(whichDriver, whereeDriver);
+            System.setProperty(whichDriver, whereDriver);
             driver = new ChromeDriver();
 //            driver = new ChromeDriver(new ChromeOptions().addArguments("-headless"));
         }
